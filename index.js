d21c2408cf9a2bf563fa39c35206277023e17cb4
Oriol Puig <oriol.puig90@gmail.com>
2016-01-03 14:38:36 +0100

    Clean index.js

--- index.js
+++ index.js
@@ -3,24 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
-// console.log(process.env.NODE_ENV);
-var env = process.env.NODE_ENV || 'development';
-
-switch (env) {
-    case 'development':
-        console.log('dev --> ' + process.env.NODE_ENV);
-        app.use(express.static(__dirname));
-        break;
-    case 'production':
-        console.log('prod --> ' + process.env.NODE_ENV);
-        app.use(express.static(__dirname + '/dist'));
-        break;
-    default:
-        console.log('DEFAULT ' + process.env.NODE_ENV);
-        app.use(express.static(__dirname + '/dist'));
-        break;
-}
-
+app.use(express.static(__dirname + '/'));
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
